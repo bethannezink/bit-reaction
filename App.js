627b2d5f7f45ae30a5d5ea62449f8355d36ed438
Bethanne Zink <bethannezink@gmail.com>
2018-01-17 23:49:03 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import LineChart from './components/LineChart.js'
+
+class App extends Component {
+  createDummyData() {
+    const data = [];
+
+    for (let x = 0; x <= 30; x++) {
+      const random = Math.random();
+      const temp = data.length > 0 ? data[data.length-1].y : 50;
+      const y = random >= .45 ? temp + Math.floor(random * 20) : temp - Math.floor(random * 20);
+      data.push({x, y});
+    }
+
+    return data;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="header">Test Line Chart</div>
+        <LineChart data={this.createDummyData()} />
+      </div>
+    );
+  }
+}
+
+export default App;
