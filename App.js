3f764430bf56ac6c128fbd0857e13504b5f7ce86
Bethanne Zink <bethannezink@gmail.com>
2018-02-08 11:49:06 -0500

    fetch coindesk data

--- App.js
+++ App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import LineChart from './components/LineChart.js'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+
   createDummyData() {
     const data = [];
 
@@ -13,6 +20,18 @@ class App extends Component {
     }
 
     return data;
+  }
+
+  componentDidMount() {
+    fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+    .then(response => response.json())
+    .then(data => {
+      let price = data.bpi.USD.rate_float;
+      console.log(data);
+      console.log(price);
+      this.setState({data});
+    });
+
   }
 
   render() {
